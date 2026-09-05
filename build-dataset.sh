59856d5916ffe0e832a6e842d9d74439fd8ad6c7
Ben Lambert <blambert@gmail.com>
2026-09-05 13:58:23 -0400

    update build script

--- build-dataset.sh
+++ build-dataset.sh
@@ -1,16 +1,31 @@
 
 
 # download the metadata files from inaturalist
-aws s3 cp s3://inaturalist-open-data/taxa.csv.gz . --no-sign-request
-aws s3 cp s3://inaturalist-open-data/observations.csv.gz . --no-sign-request
-aws s3 cp s3://inaturalist-open-data/photos.csv.gz . --no-sign-request
+aws s3 cp s3://inaturalist-open-data/taxa.csv.gz data/ --no-sign-request
+aws s3 cp s3://inaturalist-open-data/observations.csv.gz data/ --no-sign-request
+aws s3 cp s3://inaturalist-open-data/photos.csv.gz data/ --no-sign-request
 
 # do the filtering
-# uv run src/inat_data/filter.py
-
-# get common names (need LM or LM API)
+uv run src/inat_data/filter.py
 
 # download images
+uv run src/inat_data/download_pics.py data/plant_pics.tsv
+
+# upload to an unannotated dataset on HF?
+
+
+# use a LM to annotate (some of?) the imgs
+
+
+# train and build a classifier to annotate the rest of the images?
+
+
+# re-upload the clean images?
+
+
+
+
+# get common names (need LM or LM API)
 
 # (optional: do image classification)
 
