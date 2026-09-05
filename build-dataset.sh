b4b55a7e3b282b61e8b58dc2c4e2024af7457e13
Ben Lambert <blambert@gmail.com>
2026-09-05 14:04:09 -0400

    refactor: rename package/project from inat-data to ne-plants

--- build-dataset.sh
+++ build-dataset.sh
@@ -5,10 +5,10 @@ aws s3 cp s3://inaturalist-open-data/observations.csv.gz data/ --no-sign-request
 aws s3 cp s3://inaturalist-open-data/photos.csv.gz data/ --no-sign-request
 
 # do the filtering
-uv run src/inat_data/filter.py
+uv run src/ne_plants/filter.py
 
 # download images
-uv run src/inat_data/download_pics.py data/plant_pics.tsv
+uv run src/ne_plants/download_pics.py data/plant_pics.tsv
 
 # upload to an unannotated dataset on HF?
 
